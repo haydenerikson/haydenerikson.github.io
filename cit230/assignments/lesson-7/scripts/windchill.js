e46aa76db9b2678018937234ed43519de2929266
haydenerikson <38664421+haydenerikson@users.noreply.github.com>
2018-06-09 18:09:54 -0600

    last fix
    hopefully

--- cit230/assignments/lesson-7/scripts/windchill.js
+++ cit230/assignments/lesson-7/scripts/windchill.js
@@ -3,5 +3,5 @@ var s = 20;
 var f = 35.74 + .6215 * t - 35.75 * Math.pow(s,0.16) + .5275 * t * Math.pow(s,0.16);
 document.getElementById("windSpeed").innerHTML = s;
 document.getElementById("temp").innerHTML = t;
-document.getElementById("windChillFctor").innerHTML = f.toFixed(2);
+document.getElementById("windChill").innerHTML = f.toFixed(2);
 
