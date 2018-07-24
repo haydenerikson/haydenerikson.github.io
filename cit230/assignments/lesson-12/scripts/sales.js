92ce0a78280bde26c42eb7c67ea37bebcb317d8b
haydenerikson <38664421+haydenerikson@users.noreply.github.com>
2018-07-23 19:15:04 -0600

    image fix

--- cit230/assignments/lesson-12/scripts/sales.js
+++ cit230/assignments/lesson-12/scripts/sales.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
     $(".description").hide();
     $("#slrace").click(function(){
-        $("#descript2on, #descript3on").hide("slow");
-        $("#descript1on").slideToggle("slow");
+        $("#descript2on, #descript3on").hide("fast");
+        $("#descript1on").slideToggle("fast");
     });
 });
 
 $(document).ready(function(){
     $("#fueldex").click(function(){
-        $("#descript1on, #descript3on").hide("slow");
-        $("#descript2on").slideToggle("slow");
+        $("#descript1on, #descript3on").hide("fast");
+        $("#descript2on").slideToggle("fast");
     });
 });
 
 $(document).ready(function(){
     $("#sessiondh").click(function(){
-        $("#descript1on, #descript2on").hide("slow");
-        $("#descript3on").slideToggle("slow");
+        $("#descript1on, #descript2on").hide("fast");
+        $("#descript3on").slideToggle("fast");
     });
 });
 
